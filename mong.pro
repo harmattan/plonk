7a02e47dac91d4de47cc7aaf04356f88c3df108d
Cornelius Hald <cornelius.hald@tourschall.com>
2011-05-29 12:24:39 +0200

    Switched to OpenGL for increased performance.

--- mong.pro
+++ mong.pro
@@ -1,4 +1,4 @@
-QT += declarative
+QT += declarative opengl
 
 SOURCES += src/main.cpp
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-
 #include <QtCore>
 #include <QtGui>
 #include <QtDeclarative>
+#include <QGLWidget>
 
 #include "qdeclarativetoucharea.h"
 
@@ -12,12 +12,25 @@ int main(int argc, char *argv[])
     /* Enable support for TouchArea and TouchPoint */
     QDeclarativeTouchArea::registerQML();
 
-    /* Prepare the QML view and load the game content */
+    /* Using OpenGL for increased performance */
     QDeclarativeView view;
+    QGLFormat format = QGLFormat::defaultFormat();
+    format.setSampleBuffers(true);
+    QGLWidget *glWidget = new QGLWidget(format);
+    glWidget->setAutoFillBackground(false);
+
+    /* TODO: There might be settings for even better performance */
+    view.setViewport(glWidget);
+    view.setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+    view.setAttribute(Qt::WA_OpaquePaintEvent);
+    view.setAttribute(Qt::WA_NoSystemBackground);
+    view.viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
+    view.viewport()->setAttribute(Qt::WA_NoSystemBackground);
+
+    /* Prepare the QML view and load the game content */
     view.setSource(QUrl("qrc:qml/mong/main.qml"));
     view.setResizeMode(QDeclarativeView::SizeRootObjectToView);
     view.showFullScreen();
-
 
     return app.exec();
 }
