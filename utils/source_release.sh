f3ad3f6156b4371281413ca2a03151bbde01347c
Thomas Perl <m@thp.io>
2011-06-03 10:40:38 +0200

    Create propre source tarballs

--- utils/source_release.sh
+++ utils/source_release.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-git archive --prefix=mong/ --format=zip master -o ../mong_source.zip
+if [ ! -f mong.spec ]; then
+    echo "mong.spec not found. wrong directory?"
+    exit 1
+fi
+
+VERSION=`cat mong.spec | grep ^Version: | cut -d: -f2`
+
+echo -n "Building tarball for version $VERSION..."
+
+git archive --prefix=mong/ --format=tar master | gzip >mong-$VERSION.tar.gz
+
+echo "done."
 
